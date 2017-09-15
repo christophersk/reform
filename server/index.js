ba19a889cc817de54176cca2e4c165e14bf4246e
Chris Skene <cnskene@gmail.com>
2017-09-14 22:04:19 -0500

    updated port variable

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@
 const app = require('./app');
 const db = require('./db');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, err => {
   if (err) throw err;
